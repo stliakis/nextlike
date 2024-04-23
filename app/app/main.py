b680bebceee4e3e059adbfe21c21f9b9378be987
stefanos <stliakis@gmail.com>
2024-04-23 13:43:50 +0300

    Added middleware to calculate and log processing time for each request.

--- app/app/main.py
+++ app/app/main.py
@@ -1,6 +1,9 @@
 import os
+import time
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from starlette.requests import Request
 
 from app.logger import initialize_logger
 from app.settings import get_settings
@@ -17,6 +20,15 @@ initialize_logger()
 os.environ["OPENAI_API_KEY"] = get_settings().OPENAI_API_KEY
 
 app = FastAPI(redoc_url="/docs", docs_url="/docs/swagger")
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    response = await call_next(request)
+    print(f"[{request.url}] took {round((time.time() - start_time) * 1000)}ms")
+    return response
+
 
 app.include_router(items.router)
 app.include_router(events.router)
