57a7f0352b069c28c423aacc080c41af5b696724
stefanos <stliakis@gmail.com>
2024-07-09 16:48:47 +0300

    feat: improve database cleanup tasks performance
    This commit enhances the database cleanup tasks, specifically for events and recommendations history. The changes include batching the deletion operations and flushing the database every 1000 operations to reduce memory usage and increase efficiency. Furthermore, the 'cleanup_lone_person_events' task has been updated to utilize settings for time interval and minimum count values.

--- app/app/tasks/beat.py
+++ app/app/tasks/beat.py
@@ -12,34 +12,68 @@ from app.utils.base import parse_time_string
 @celery_app.task
 def cleanup_events():
     with Database() as db:
-        events = m.Event.objects(db).filter(
-            m.Event.created < datetime.datetime.now() - datetime.timedelta(
-                seconds=parse_time_string(get_settings().EVENTS_CLEANUP_AFTER))
+        events = (
+            m.Event.objects(db)
+            .filter(
+                m.Event.created
+                < datetime.datetime.now()
+                - datetime.timedelta(
+                    seconds=parse_time_string(get_settings().EVENTS_CLEANUP_AFTER)
+                )
+            )
+            .yield_per(1000)
         )
-        for event in events:
+
+        for i, event in enumerate(events):
             event.delete(db)
+            if i % 1000 == 0:
+                db.flush()
+
+        db.flush()
 
 
 @celery_app.task
 def cleanup_recommendations_history():
     with Database() as db:
-        recommendations = m.SearchHistory.objects(db).filter(
-            m.SearchHistory.created < datetime.datetime.now() - datetime.timedelta(
-                seconds=parse_time_string(get_settings().RECOMMENDATIONS_HISTORY_CLEANUP_AFTER))
+        recommendations = (
+            m.SearchHistory.objects(db)
+            .filter(
+                m.SearchHistory.created
+                < datetime.datetime.now()
+                - datetime.timedelta(
+                    seconds=parse_time_string(
+                        get_settings().RECOMMENDATIONS_HISTORY_CLEANUP_AFTER
+                    )
+                )
+            )
+            .yield_per(1000)
         )
-        for recommendation in recommendations:
+
+        for i, recommendation in enumerate(recommendations):
             recommendation.delete(db)
+            if i % 1000 == 0:
+                db.flush()
+
+        db.flush()
 
 
 @celery_app.task
 def cleanup_lone_person_events():
     with Database() as db:
-        db.execute(text("""
+        db.execute(
+            text(
+                """
         delete
         from event
         where person_external_id in (select person_external_id
                                      from event
-                                     where created < CURRENT_DATE - INTERVAL '3 days'
+                                     where created < CURRENT_DATE - INTERVAL '%i days'
                                      group by person_external_id
-                                     having count(*) <= 2)
-        """))
+                                     having count(*) <= %i)
+        """
+                % (
+                    get_settings().EVENTS_CLEANUP_LONE_EVENTS_AFTER,
+                    get_settings().EVENTS_CLEANUP_LONE_EVENTS_MIN_COUNT,
+                )
+            )
+        )
