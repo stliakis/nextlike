9acccbffe5d959b882ee69996c4b72f991120711
stefanos <stliakis@gmail.com>
2024-07-17 12:39:32 +0300

    feat: optimize database cleanup tasks
    Refactored the database cleanup tasks in beat.py to use direct SQL deletion queries instead of ORM methods. This change significantly improves the performance of the cleanup tasks by eliminating the need to iterate over and delete records one by one.

--- app/app/tasks/beat.py
+++ app/app/tasks/beat.py
@@ -12,49 +12,35 @@ from app.utils.base import parse_time_string
 @celery_app.task
 def cleanup_events():
     with Database() as db:
-        events = (
-            m.Event.objects(db)
-            .filter(
-                m.Event.created
-                < datetime.datetime.now()
-                - datetime.timedelta(
-                    seconds=parse_time_string(get_settings().EVENTS_CLEANUP_AFTER)
+        db.execute(
+            text(
+                """
+                delete
+                from event
+                where created < CURRENT_DATE - INTERVAL '%i seconds'
+                """
+                % (
+                    parse_time_string(get_settings().EVENTS_CLEANUP_AFTER)
                 )
             )
-            .yield_per(1000)
         )
-
-        for i, event in enumerate(events):
-            event.delete(db)
-            if i % 1000 == 0:
-                db.flush()
-
-        db.flush()
 
 
 @celery_app.task
 def cleanup_recommendations_history():
     with Database() as db:
-        recommendations = (
-            m.SearchHistory.objects(db)
-            .filter(
-                m.SearchHistory.created
-                < datetime.datetime.now()
-                - datetime.timedelta(
-                    seconds=parse_time_string(
-                        get_settings().RECOMMENDATIONS_HISTORY_CLEANUP_AFTER
-                    )
+        db.execute(
+            text(
+                """
+                delete
+                from search_history
+                where created < CURRENT_DATE - INTERVAL '%i seconds'
+                """
+                % (
+                    parse_time_string(get_settings().RECOMMENDATIONS_HISTORY_CLEANUP_AFTER)
                 )
             )
-            .yield_per(1000)
         )
-
-        for i, recommendation in enumerate(recommendations):
-            recommendation.delete(db)
-            if i % 1000 == 0:
-                db.flush()
-
-        db.flush()
 
 
 @celery_app.task
@@ -63,16 +49,15 @@ def cleanup_lone_person_events():
         db.execute(
             text(
                 """
-        delete
-        from event
-        where person_external_id in (select person_external_id
-                                     from event
-                                     where created < CURRENT_DATE - INTERVAL '%i days'
-                                     group by person_external_id
-                                     having count(*) <= %i)
-        """
-                % (
-                    get_settings().EVENTS_CLEANUP_LONE_EVENTS_AFTER,
+                delete
+                from event
+                where person_external_id in (select person_external_id
+                                             from event
+                                             where created < CURRENT_DATE - INTERVAL '%i seconds'
+                                             group by person_external_id
+                                             having count(*) <= %i)
+                """ % (
+                    parse_time_string(get_settings().EVENTS_CLEANUP_LONE_EVENTS_AFTER),
                     get_settings().EVENTS_CLEANUP_LONE_EVENTS_MIN_COUNT,
                 )
             )
